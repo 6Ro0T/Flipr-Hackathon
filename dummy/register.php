b40d7af98013fbcb6d880d8073f80d87ae60256e
Sripathikumar T S <68003201+Art3mis-005@users.noreply.github.com>
2021-08-28 12:08:47 +0530

    just updated 12:09PM, Sat

--- dummy/register.php
+++ dummy/register.php
@@ -7,7 +7,7 @@
 
 <link rel="icon" href="favicon.ico" type="image/x-icon"/>
 
-<title>MYNCC:Login</title>
+<title>MYNCC:Register</title>
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
 <!-- Bootstrap Core and vandor -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -17,90 +17,32 @@
 
 </head>
 <body class="font-muli theme-cyan gradient">
-<form action="index.php" method="Post">
+<form action="#" method="Post">
 
     <div class="row">
         <div class="col-md-5 offset-md-4">
             <div class="card-body">
                 <div class="text-center">
                     <a class="header-brand" href="../index.php"><i class="fa fa-graduation-cap brand-logo" style="font-size: 30px;"></i></a>
-                    <div class="card-title mt-3"> Login</div>
-                    
+                    <div class="card-title mt-3"> Registeration </div>    
                 </div>
                 <div class="form-group">
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"name="email" required>
                 </div>
                 <div class="form-group">
-                    
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"name="passwd" required>
-                    <label class="form-label"><a href="forgot-password.html" class="float-right small">I forgot password</a></label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="passwd" required>
                 </div>
                 <div class="form-group">
-                    <label class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" />
-                    <span class="custom-control-label">Remember me</span>
-                    </label>
+                    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Confirm Password" name="passwd" required>
                 </div>
                 <div class="text-center">
-                    <button class="btn btn-primary btn-block" title="">Sign in</button>
-                    <?php
-					
-					if (isset($_GET["msg"]) == 'failed') {
-						echo "Incorrect username or password";
-}       			?>
-                </div>
-				
+                    <button class="btn btn-primary btn-block" title="">Register</button>
+                </div>	
             </div>
         </div>        
     </div>
 
 </form>
-
-<?php
-
-   include("conn.php");
-   session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($conn,$_POST['email']);
-      $passwd=$_POST['passwd'];
-      $mypassword=hash('sha256',$passwd );
-      echo $mypassword;
-      
-      $sql = "SELECT email FROM student WHERE email = '$myusername' and password = '$mypassword'";
-      $result = mysqli_query($conn,$sql);
-      $row = mysqli_fetch_array($result);
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-      $sql1="select division from student where email='$myusername'";
-      $result1=mysqli_query($conn,$sql1);
-      $row1=mysqli_fetch_array($result1);
-      $value=$row1["division"];
-      if($count == 1) {
-         //session_register("username");
-         $_SESSION['login_user'] = $myusername;
-         $_SESSION['role']=$value;
-         if($value=='navy'){
-         
-            header("location: navy/navy.php");
-         }
-         elseif($value=='army'){
-             header("location: army/army.php");
-         }
-         elseif($value=='airforce'){
-             header("location: /airforce/airforce.php");
-         }
-      }else {
-           header("location:index.php?msg=failed");
-		 
-      }
-   }
-
-?>
-
 <!-- Start Main project js, jQuery, Bootstrap -->
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
